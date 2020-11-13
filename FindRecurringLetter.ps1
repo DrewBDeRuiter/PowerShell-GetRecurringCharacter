214f21ebf7b11ef89dff0919e6bbde607b1a3a0d
DrewBDeRuiter <drewbderuiter@gmail.com>
2020-11-13 16:47:24 -0500

    Add files via upload

---
+++ FindRecurringLetter.ps1
@@ -0,0 +1,19 @@
+﻿
+
+$charDictionary = New-Object 'system.collections.generic.dictionary[string,string]'
+
+    $longString = "abcdedcba"
+    $longStringArray = $longString.ToCharArray()
+    foreach($letter in $longStringArray)
+    {
+        if (!$charDictionary.ContainsKey($letter)) 
+        {
+            $charDictionary.Add($letter,$letter)
+        }
+        else
+        {
+            Write-Output $letter
+            break
+        }
+
+    }
